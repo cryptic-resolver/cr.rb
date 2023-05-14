dd074854a62531fc1975a8088419f273828e2612
Aoran Zeng <ccmywish@qq.com>
2023-05-14 22:52:10 +0800

    Fix helper to verify specs

--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -4,14 +4,16 @@ require_relative '../lib/cr'
 Rainbow.enabled = false
 
 Reslvr = CrypticResolver::Resolver.new
-
 Result = StringIO.new
 ORIG_STDOUT = STDOUT
 
 def lookup(word)
   Reslvr.resolve_word word
   Result.rewind
-  return Result.read
+  cont = Result.read
+  Result.rewind
+  Result.truncate(0)
+  return cont
 end
 
 describe CrypticResolver do
@@ -20,19 +22,14 @@ describe CrypticResolver do
     $stdout = Result
   end
 
-  after do
-    $stdout = ORIG_STDOUT
-  end
-
   it "run_first_to_pull_dictionaries_for_ci" do
     # just a random command to let CI pull dictionaries first
     # result = cr("-c", display: true)
   end
 
+
   it "lookup 'emacs'" do
-
     result = lookup 'emacs'
-
     _(result).must_equal <<~RES
       From: cryptic_computer
 
@@ -46,9 +43,45 @@ describe CrypticResolver do
   end
 
 
-  it "after" do
+  it "lookup 'jpg'" do
+    result = lookup 'jpg'
+    _(result).must_equal <<~RES
+    From: cryptic_computer
+    JPG redirects to JPEG
+
+      JPEG: Joint Photographic Experts Group
+
+      Introduced in 1992. A commonly used method of lossy compression for digital images
+
+    RES
+  end
+
+
+  it "lookup 'loc" do
+    result = lookup 'loc'
+    _(result).must_equal <<~RES
+    From: cryptic_computer
+    loc redirects to sloc
+
+      sloc: Source Lines of Code
+
+    RES
+  end
+
+
+  it "lookup 'bcd'" do
+    result = lookup 'bcd'
+    _(result).must_equal <<~RES
+    From: cryptic_computer
+    bcd redirects to BCDIC
+
+      BCDIC: Binary-Coded Decimal Interchange Code
+
+      6-bit alphanumeric codes that represented numbers, upper-case letters and special characters.
+
+    SEE ALSO EBCDIC
+
+    RES
   end
 
 end
-
-puts "=> After specs, everything is fine ~"
